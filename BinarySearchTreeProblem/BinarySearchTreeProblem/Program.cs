78acfe3f51cf4039188a42da7cf425a833ea9467
prashantmagare <prashantmagare3010@gmail.com>
2022-05-30 00:04:24 +0530

    "Added - BinarySearchTree"

---
+++ BinarySearchTreeProblem/BinarySearchTreeProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BinarySearch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Binary Search Program");
+        }
+    }
+}
