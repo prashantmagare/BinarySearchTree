cf448caa5e78a4df15eb38289a14404c393c35b8
prashantmagare <prashantmagare3010@gmail.com>
2022-05-30 00:09:09 +0530

    "Added - UC2_BST"

--- BinarySearchTreeProblem/BinarySearchTreeProblem/Program.cs
+++ BinarySearchTreeProblem/BinarySearchTreeProblem/Program.cs
@@ -7,11 +7,21 @@ namespace BinarySearch
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Binary Search Program");
-            MyBinaryNode<int> bst = new MyBinaryNode<int>(3);
-            bst.Insert(56);
+            MyBinaryNode<int> bst = new MyBinaryNode<int>(56);
             bst.Insert(30);
             bst.Insert(70);
+            bst.Insert(22);
+            bst.Insert(40);
+            bst.Insert(60);
+            bst.Insert(95);
+            bst.Insert(11);
+            bst.Insert(65);
+            bst.Insert(3);
+            bst.Insert(16);
+            bst.Insert(63);
+            bst.Insert(67);
             bst.Display();
+            bst.GetSize();
         }
     }
 }
